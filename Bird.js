6f146650bd8f8ac2722249950e5cd5bf18a8bd2c
Manya Sharma <manyasharma101007@gmail.com>
2020-12-09 18:00:49 +0530

    Angry_Birds_6

--- Bird.js
+++ Bird.js
@@ -2,11 +2,22 @@ class Bird extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
     this.image = loadImage("sprites/bird.png");
+    this.smoke = loadImage("sprites/smoke.png");
+    this.tragectry = [];
+
+
   }
 
   display() {
     //this.body.position.x = mouseX;
     //this.body.position.y = mouseY;
+
+    var position = [this.body.position.x,this.body.position.y];
+    this.tragectry.push(position)
+
+    for(var i = 0; i <= this.tragectry.length - 1; i++) {
+      image(this.smoke,this.tragectry[i][0],this.tragectry[i][1]);
+    }
 
     super.display();
   }
